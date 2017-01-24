9e3f6f7070a108e0455fa60b59dff3e076ff6169
hecsaen <hectorsaenznino@gmail.com>
2017-01-24 03:39:47 +0100

    Add files via upload

---
+++ javascript/scripts.js
@@ -0,0 +1,34 @@
+var myIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("sliderDiv");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    myIndex++;
+    if (myIndex > x.length) {
+        myIndex = 1
+    }
+    x[myIndex - 1].style.display = "block";
+    setTimeout(carousel, 3000);
+}
+
+function hasClass(el, className) {
+    if (el.classList) return el.classList.contains(className)
+    else return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'))
+}
+
+function addClass(el, className) {
+    if (el.classList) el.classList.add(className)
+    else if (!hasClass(el, className)) el.className += " " + className
+}
+
+function removeClass(el, className) {
+    if (el.classList) el.classList.remove(className)
+    else if (hasClass(el, className)) {
+        var reg = new RegExp('(\\s|^)' + className + '(\\s|$)')
+        el.className = el.className.replace(reg, ' ')
+    }
+}
